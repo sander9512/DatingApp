23acea564b2bc906484a634f04a8d3ef9db9d35a
Sander Schep <s.c.schep@gmail.com>
2020-12-07 04:09:04 +0100

    Angular client skeleton + API Cors added

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
