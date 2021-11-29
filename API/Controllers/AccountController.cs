9716249213ffe15fff6d06901db6fd8b83e97624
Sander Schep <s.c.schep@gmail.com>
2021-11-29 16:52:14 +0100

    Added photo upload functionality

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,7 +47,9 @@ namespace API.Controllers
         [HttpPost("Login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
             
-            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
 
             if(user == null) return (Unauthorized("Invalid username"));
 
@@ -59,7 +61,8 @@ namespace API.Controllers
             }
              return new UserDto {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {
       map((res: User) => {
         const user = res;
         if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -31,13 +30,13 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
   }
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
