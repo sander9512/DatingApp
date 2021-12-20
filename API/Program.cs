4b7389b28e47293cf01b9b9459165c88f564dcc7
Sander Schep <s.c.schep@gmail.com>
2021-12-20 17:11:12 +0100

    Updated to dotnet6

--- API/Program.cs
+++ API/Program.cs
@@ -1,47 +1,92 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace API
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to container
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSignalR();
 
-            try {
-                var context = services.GetRequiredService<DataContext>();
-                var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-                await context.Database.MigrateAsync();
-                
-                await Seed.SeedUsers(userManager, roleManager);
-            } 
-            catch (Exception ex) {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occured during migration");
-            }
-            await host.RunAsync();
-        }
+//configure HTTP request pipeline
+var app = builder.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowCredentials().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+    await context.Database.MigrateAsync();
+
+    await Seed.SeedUsers(userManager, roleManager);
 }
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
+await app.RunAsync();
+
+//.NET 5 Program.cs
+
+// namespace API
+// {
+//     public class Program
+//     {
+//         public static async Task Main(string[] args)
+//         {
+//             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+//             var host = CreateHostBuilder(args).Build();
+//             using var scope = host.builder.services.CreateScope();
+//             var builder.services = scope.ServiceProvider;
+
+//             try {
+//                 var context = builder.services.GetRequiredService<DataContext>();
+//                 var userManager = builder.services.GetRequiredService<UserManager<AppUser>>();
+//                 var roleManager = builder.services.GetRequiredService<RoleManager<AppRole>>();
+//                 await context.Database.MigrateAsync();
+
+//                 await Seed.SeedUsers(userManager, roleManager);
+//             } 
+//             catch (Exception ex) {
+//                 var logger = builder.services.GetRequiredService<ILogger<Program>>();
+//                 logger.LogError(ex, "An error occured during migration");
+//             }
+//             await host.RunAsync();
+//         }
+
+//         public static IHostBuilder CreateHostBuilder(string[] args) =>
+//             Host.CreateDefaultBuilder(args)
+//                 .ConfigureWebHostDefaults(webBuilder =>
+//                 {
+//                     webBuilder.UseStartup<Startup>();
+//                 });
+//     }
+// }
